1a2131c27716a91f0b53c939305929f49665e926
Xventurer1014 <81269669+Xventurer1014@users.noreply.github.com>
2025-05-14 17:17:52 +0000

    lol

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@ int main(void) {
     /*printf("The array is percent sign ld bytes long.\n", sizeof(myArray));*/
     printf("The location of the first array is \t%p\n", myArray);
     printf("The location of the first element is \t%p\n", &myArray[0]);
-    printf("The location of the secong element is at \t%p\n", &myArray[1]);
+    printf("The location of the second element is at \t%p\n", &myArray[1]);
     printf("The largest value of a float is %E\n", __FLT_MAX__);
-    printf("The shortest value of a float is %E\n", __FLT_MAX__);
+    printf("The shortest value of a float is %E\n", __FLT_MIN__);
 }
