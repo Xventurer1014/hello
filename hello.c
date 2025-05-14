ae3a4a5e19624454b0774890d62423f070d37357
Xventurer1014 <81269669+Xventurer1014@users.noreply.github.com>
2025-05-14 17:09:46 +0000

    ghghg

--- hello.c
+++ hello.c
@@ -6,6 +6,11 @@ int myArray[] = {0, 2, 3, 4, 5};
 
 int main(void) {
     printf("Hello, World!\n");
-    printf("Third value of data array is %d.\n", myArray[2]);
-    printf("The array is %d bytes long.\n", sizeof(myArray));
+    printf("The value of data array is %d.\n", myArray[2]);
+    /*printf("The array is percent sign ld bytes long.\n", sizeof(myArray));*/
+    printf("The location of the first array is \t%p\n", myArray);
+    printf("The location of the first element is \t%p\n", &myArray[0]);
+    printf("The location of the secong element is at \t%p\n", &myArray[1]);
+    printf("The largest value of a float is %E\n", __FLT_MAX__);
+    printf("The shortest value of a float is %E\n", __FLT_MAX__);
 }
